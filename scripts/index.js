ac15ed5a909bd4548b4d0e41d9f81bf09e900ad3
Joe Byrne <joe.byrne@Creative5-iMac.local>
2020-03-04 15:52:31 +0000

    styling

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,8 @@
 window.addEventListener("load", () => {
+  var timezone = document.querySelector(".location-timezone");
+  var temp = document.querySelector(".temperature");
+  var summary = document.querySelector(".description");
+  const x = document.getElementsByTagName("BODY")[0];
   // getting the time...
 
   var d = new Date();
@@ -16,6 +20,8 @@ window.addEventListener("load", () => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
+      cold = "fbewof";
+
       const proxy = "http://cors-anywhere.herokuapp.com/";
       const api = `${proxy}https://api.darksky.net/forecast/8d72407c3fc165393d7389065b4ae247/${lat},${long}`;
 
@@ -24,14 +30,23 @@ window.addEventListener("load", () => {
           return res.json();
         })
         .then(data => {
-          console.log(data);
-
           const { temperature, icon } = data.currently;
 
-          console.log("temperature", temperature);
-          console.log("icon", icon);
-          console.log("timeZone", data.timezone);
-          console.log("summary", data.hourly.summary);
+          //Timezone added the the HTML
+          timezone.innerHTML = data.timezone;
+
+          //Temperature added the the HTML
+          var tempC = (temperature - 32) * (5 / 9);
+          temp.innerHTML = Math.ceil(tempC) + " ˚C";
+
+          //Summary added the the HTML
+          summary.innerHTML = data.hourly.summary;
+
+          if (temperature > 45) {
+            temp.classList.add("hot");
+          } else {
+            temp.classList.add("cold");
+          }
         });
     });
   }
